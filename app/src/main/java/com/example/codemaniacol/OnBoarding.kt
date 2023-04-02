c91e48f60bd5db5ff9bd854794afb8d3e31e1214
Yzvan <kyzmin.egor08@gmail.com>
2023-04-02 22:46:24 +0300

    OnBoarding

--- app/src/main/java/com/example/codemaniacol/OnBoarding.kt
+++ app/src/main/java/com/example/codemaniacol/OnBoarding.kt
@@ -2,11 +2,38 @@ package com.example.codemaniacol
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
+import com.example.codemaniacol.databinding.ActivityOnBoardingBinding
 
 class OnBoarding : AppCompatActivity() {
+    lateinit var OnBoardingActivity : ActivityOnBoardingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        OnBoardingActivity = ActivityOnBoardingBinding.inflate(layoutInflater)
+        setContentView(OnBoardingActivity.root)
+        val button2 = findViewById<Button>(R.id.button2)
+
         setContentView(R.layout.activity_on_boarding)
-        val list = listOf(OnBoard())
+        val list = listOf(OnBoard("Без теории, только практика\n" +
+                "Вы не платите за лекции и теоретический материал, ведь все это можно найти в интернете бесплатно",R.drawable.blog_post_bro_1),
+            OnBoard("Без теории, только практика\n" +
+                "Вы не платите за лекции и теоретический материал, ведь все это можно найти в интернете бесплатно",R.drawable.novelist_writing_bro_1),
+            OnBoard("Обучение онлайн из любой точки мира\n" +
+                    "Наше обучение изначально создавалось как дистанционное",R.drawable.blogging_bro_1)
+        )
+        OnBoardingActivity.Viewpager.adapter = ViewPagerAdapter(list)
+        OnBoardingActivity.Viewpager.registerOnPageChangeCallback(object :ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                if (position == 2){
+                    button2.text = "Начать"
+                }
+                super.onPageSelected(position)
+            }
+
+
+
+        })
     }
 }
---
+++ app/src/main/java/com/example/codemaniacol/ViewPagerAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.codemaniacol
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+
+class ViewPagerAdapter(val list: List<OnBoard>): RecyclerView.Adapter<ViewPagerAdapter.VH>() {
+    class VH(itenView:View):RecyclerView.ViewHolder(itenView) {
+        val OnBoardText = itenView.findViewById<TextView>(R.id.OnBoardTEXT)
+        val OnBoardImg = itenView.findViewById<ImageView>(R.id.OnboardIMG)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        return VH(LayoutInflater.from(parent.context).inflate(R.layout.on_board_item,parent,false))
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+
+        holder.OnBoardText.text = list[position].text
+        holder.OnBoardImg.setImageDrawable(ContextCompat.getDrawable(holder.itemView.context, list[position].img))
+    }
+}
